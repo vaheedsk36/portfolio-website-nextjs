9713e298ed827af242a87fd1fb1fb20c7e7cab4f
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-08-14 23:24:49 +0530

    wip-layout

--- app/ArticlesCard.jsx
+++ app/ArticlesCard.jsx
@@ -1,21 +1,9 @@
-import React from "react";
-import { Card } from "antd";
-const { Meta } = Card;
-const ArticlesCard = ({ title, coverImage, slug }) => (
-  <Card
-    hoverable
-    style={{
-      width: 240,
-    }}
-    cover={
-      <img
-        alt="article image"
-        src={coverImage}
-      />
-    }
-    onClick={() => alert("working")}
-  >
-    <Meta title={title} description="www.instagram.com" />
-  </Card>
-);
-export default ArticlesCard;
+import React from 'react'
+
+const ArticlesCard = () => {
+  return (
+    <div>ArticlesCard</div>
+  )
+}
+
+export default ArticlesCard
--- app/MainLayout.jsx
+++ app/MainLayout.jsx
@@ -1,79 +1,9 @@
-import React, { useState } from 'react';
-import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from '@ant-design/icons';
-import { Layout, Menu, Button, theme } from 'antd';
-const { Header, Sider, Content } = Layout;
+import React from 'react'
+
 const MainLayout = ({children}) => {
-  const [collapsed, setCollapsed] = useState(false);
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
   return (
-    <Layout
-    style={{
-      margin: '0px 0px',
-    }}
-    >
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-        />
-      </Sider>
-      <Layout>
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        >
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          />
-        </Header>
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 1000,
-            background: colorBgContainer,
-          }}
-        >
-          
-        </Content>
-      </Layout>
-    </Layout>
-  );
-};
-export default MainLayout;
+    <div>{children}</div>
+  )
+}
+
+export default MainLayout
