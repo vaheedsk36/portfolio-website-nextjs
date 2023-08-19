9a6fd2e0913eafb36384350925489cacb7272838
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-08-19 11:56:07 +0530

    wip

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require("path");
 
-module.exports = nextConfig
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src", "styles")],
+  },
+};
