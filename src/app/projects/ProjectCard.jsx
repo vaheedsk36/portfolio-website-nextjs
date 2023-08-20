0bd342e1d338a86789f9ece1f486591b68c25de0
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-08-20 19:58:49 +0530

    basic-layout-added-for-projects

--- src/app/projects/ProjectCard.jsx
+++ src/app/projects/ProjectCard.jsx
@@ -1,21 +1,48 @@
-"use client"
+"use client";
 import React from "react";
 import {
   Card,
   CardBody,
   Image,
   Heading,
+  Stack,
+  Text,
+  Button,
+  Link,
 } from "@chakra-ui/react";
 
-const ProjectCard = () => {
+const ProjectCard = (props) => {
+  const { title, techstack, desc, link, source, image } = props.data;
   return (
     <>
-      <Card>
+      <Card width="25%" m={3}>
         <CardBody>
-          <Image objectFit="cover" src="" alt="Chakra UI" />
-          <Heading size="md" my={3}>
-            Project heading
-          </Heading>
+          <Image
+            objectFit="cover"
+            src={`images/${image}.png`}
+            alt="project-img"
+          />
+          <Stack mt="6" spacing="3">
+            <Heading size="md">{title}</Heading>
+
+            <Text>
+              <b>Technologies used:</b>
+              {techstack}
+            </Text>
+            <Text>{desc}</Text>
+            <div>
+              <Button colorScheme="gray">
+                <Link href={link} target="_blank">
+                  Open
+                </Link>
+              </Button>
+              <Button colorScheme="gray" mx={3}>
+                <Link href={source} target="_blank">
+                  Github
+                </Link>
+              </Button>
+            </div>
+          </Stack>
         </CardBody>
       </Card>
     </>
--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
+"use client";
+import React from "react";
 import ProjectCard from "./ProjectCard";
+import { Heading, SimpleGrid } from "@chakra-ui/react";
 
 const Projects = () => {
+  const projectsData = [
+    {
+      title: "Weather App",
+      image: "weather-app",
+      techstack: "HTML,CSS,JS",
+      desc: `This website empowers users to effortlessly search for weather conditions in any location and instantly access current weather details based on their geographical location.`,
+      link: "https://vaheedsk36.github.io/weather-app/",
+      source: "https://github.com/vaheedsk36/weather-app",
+    },
+    {
+      title: "Landing Page",
+      image: "landing-page",
+      techstack: "HTML,CSS,JS,Bootstrap",
+      desc: `Made using the vanilla js and bootstrap. This project is a demo landing page for a frontend bootcamp.`,
+      link: "https://vaheedsk36.github.io/demo-frontend-bootcamp",
+      source: "https://github.com/vaheedsk36/demo-frontend-bootcamp",
+    },
+  ];
   return (
     <>
-    <ProjectCard/>
-    </>
-  )
-}
+      <div my={3}>
+        <Heading size="lg" textAlign="center" my={3} color="white">
+          My Projects
+        </Heading>
 
-export default Projects
+        <div
+          style={{
+            display: "flex",
+          }}
+        >
+          {projectsData.map((data, index) => {
+            return <ProjectCard {...{ data }} key={index} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Projects;
