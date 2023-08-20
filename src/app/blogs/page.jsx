08d7223b290b14cede4bcc2d79f7437b1d9d2b5a
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-08-20 10:58:47 +0530

    loader-added-in-articles-section

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,10 +1,11 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import ArticlesCard from "./ArticlesCard";
-import { SimpleGrid, Heading } from "@chakra-ui/react";
+import { SimpleGrid, Heading, Spinner } from "@chakra-ui/react";
 
 const Blogs = () => {
   const [articlesData, setArticlesData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const query = `
     query GetUserArticles($page: Int = 0) {
@@ -41,22 +42,43 @@ const Blogs = () => {
   useEffect(() => {
     getArticlesData().then((data) => {
       setArticlesData(data);
+      setIsLoading(false);
     });
   }, []);
 
   return (
     <>
-      <div mt={3}>
-        <Heading size="lg" textAlign="center" mt={3} color="white">My Articles</Heading>
-        <SimpleGrid
-          p={4}
-          spacing={4}
-          templateColumns="repeat(auto-fill, minmax(350px, 1fr))"
-        >
-          {articlesData.map((data, index) => (
-            <ArticlesCard data={data} key={index} />
-          ))}
-        </SimpleGrid>
+      <div mt={3} textAlign="center">
+        {isLoading ? (
+          <div className="blog-spinner">
+            <Spinner
+              thickness="10px"
+              speed="0.6s"
+              emptyColor="gray.200"
+              color="blue.500"
+              size="xl"
+              height={150}
+              width={150}
+              mb={3}
+            />
+            <Heading color="white">Loading...</Heading>
+          </div>
+        ) : (
+          <>
+            <Heading size="lg" textAlign="center" mt={3} color="white">
+              My Articles
+            </Heading>
+            <SimpleGrid
+              p={4}
+              spacing={4}
+              templateColumns="repeat(auto-fill, minmax(350px, 1fr))"
+            >
+              {articlesData.map((data, index) => (
+                <ArticlesCard data={data} key={index} />
+              ))}
+            </SimpleGrid>
+          </>
+        )}
       </div>
     </>
   );
