69be6b0aa7c2c19a642aefd80679a3bc8743100a
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-08-22 09:35:52 +0530

    wip

---
+++ src/app/providers.jsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { CacheProvider } from '@chakra-ui/next-js'
+import { ChakraProvider } from '@chakra-ui/react'
+
+export function Providers({ 
+    children 
+  }) {
+  return (
+    <CacheProvider>
+      <ChakraProvider>
+        {children}
+      </ChakraProvider>
+    </CacheProvider>
+  )
+}
