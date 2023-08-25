111605c77a6cc16d10266648982e6072451bd606
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-08-25 10:43:16 +0530

    wip-form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,17 +1,49 @@
 "use client";
 import React from "react";
 import ContactForm from "./ContactForm";
-import { Heading, Text } from "@chakra-ui/react";
+import { Heading, Icon, Stack, HStack, Text } from "@chakra-ui/react";
+import {
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter
+} from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+import { ImLocation } from "react-icons/im";
 
 const Contact = () => {
   return (
     <div className="container">
       <div className="contact-container">
-        <Heading fontSize="3rem">
-          Let&apos;s discuss on something <span style={{
-            color:"#D53F8C"
-          }}>cool</span> together
-        </Heading>
+        <div>
+          <Heading fontSize="2.8rem">
+            Let&apos;s discuss on something{" "}
+            <span
+              style={{
+                color: "#D53F8C",
+              }}
+            >
+              cool
+            </span>{" "}
+            together
+          </Heading>
+
+          <Stack  marginTop="4rem">
+            <Text fontSize="1.4rem" display="flex" alignItems="center" my={3}>
+              <Icon as={IoIosMail} mx={2} />
+              <span>vaheedsk36@gmail.com</span>
+            </Text>
+            <Text fontSize="1.4rem" display="flex" alignItems="center" my={3}>
+              <Icon as={ImLocation} mx={2} />
+              <span>Hyderabad, India</span>
+            </Text>
+          </Stack>
+          <HStack fontSize="2rem" mx={3} marginTop="7rem">
+            <Icon as={FaInstagram} />
+            <Icon as={FaLinkedin} mx={3} />
+            <Icon as={FaTwitter} />
+          </HStack>
+        </div>
+
         <ContactForm />
       </div>
     </div>
