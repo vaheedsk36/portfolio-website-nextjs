5228a59e0fa66b3b2c5ce37af660f5768c702785
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-08-25 20:49:10 +0530

    experience-section-cmopleted

--- src/app/experience/WorkCard.jsx
+++ src/app/experience/WorkCard.jsx
@@ -3,40 +3,45 @@ import React from "react";
 import { Card, CardBody, Image, Heading, Text } from "@chakra-ui/react";
 
 const WorkCard = (props) => {
-  const { company, duration, designation, companyImg, description } = props.data;
+  const { company, duration, designation, companyImg, description } =
+    props.data;
   return (
     <>
       <Card width="58rem" my={3}>
         <CardBody className="work-card-body">
-          <div style={{
-              marginRight:'1rem',
-              height:'12rem',
-              width:'12rem',
-              display:'flex',
-              alignItems:'center',
-              backgroundColor:'white',
-              padding:'10px'
-            }}>
-          <Image
-            objectFit="cover"
-            src={`/images/${companyImg}`}
-            alt="Company Logo"
-          />
-
+          <div
+            style={{
+              marginRight: "1rem",
+              height: "12rem",
+              width: "12rem",
+              display: "flex",
+              alignItems: "center",
+              backgroundColor: "white",
+              padding: "10px",
+            }}
+          >
+            <Image
+              objectFit="cover"
+              src={`/images/${companyImg}`}
+              alt="Company Logo"
+            />
           </div>
           <div>
-            <Heading fontSize="1.5rem">
-              {company}
-            </Heading>
+            <Heading fontSize="1.5rem">{company}</Heading>
             <Heading size="sm" my={2}>
               {duration}
             </Heading>
-            <Text size="sm" my={2} fontStyle="italic">
+            <Text size="xs" my={2} fontStyle="italic">
               {designation}
             </Text>
-            <Text size="md" >
+            <div
+              style={{
+                marginLeft: "1.5rem",
+                fontSize: "0.95rem",
+              }}
+            >
               {description}
-            </Text>
+            </div>
           </div>
         </CardBody>
       </Card>
--- src/app/experience/page.jsx
+++ src/app/experience/page.jsx
@@ -8,24 +8,59 @@ const Work = () => {
     {
       company: "1Digitalstack.ai",
       designation: "Software Engineer 1",
-      duration:"March 2022 - Present",
+      duration: "March 2022 - Present",
       companyImg: "1ds.jpg",
-      description: "",
+      description: (
+        <>
+          <ul>
+            <li>
+              I work on Reviniti, an analytics product designed for e-commerce
+              players in the bid management domain. Solely migrated the old
+              codebase from a templating engine (PugJS) to React with
+              TypeScript.
+            </li>
+            <li>
+              Engaged in developing new features, refactoring code, enhancing
+              product performance, bug fixes and contributing towards achieving
+              high scalability for the product.
+            </li>
+          </ul>
+        </>
+      ),
     },
     {
       company: "Fact.MR",
       designation: "Associate Consultant Level 01",
-      duration:"Dec 2020 - July 2021",
+      duration: "Dec 2020 - July 2021",
       companyImg: "fmr.svg",
-      description: "",
+      description: (
+        <>
+          <ul>
+            <li>
+              Engaged in creating comprehensive client-specific and syndicated
+              research reports, along with SEO-focused articles and collateral
+              materials within the domains of chemicals, materials, consumer
+              goods, and industrial goods.
+            </li>
+            <li>
+              Expertly estimated market valuations and conducted primary
+              research in collaboration with industry experts to validate data,
+              extract valuable market insights.
+            </li>
+          </ul>
+        </>
+      ),
     },
   ];
   return (
     <>
-      <div className="container" style={{
-        margin:'auto 7.2rem'
-      }}>
-        <Heading className="sub-heading" size="md" my={3} >
+      <div
+        className="container"
+        style={{
+          margin: "auto 7.2rem",
+        }}
+      >
+        <Heading className="sub-heading" size="md" my={3}>
           EXPERIENCE
         </Heading>
 
