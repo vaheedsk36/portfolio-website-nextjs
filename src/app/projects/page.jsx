49a3b757af56c7a592bf5a7c6b112840b20fe9e0
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-08-25 23:37:52 +0530

    added-radio-btns-to-projects

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,67 +1,84 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
-import { Heading, SimpleGrid } from "@chakra-ui/react";
+import { Heading, SimpleGrid, HStack, useRadioGroup } from "@chakra-ui/react";
+import RadioCard from "../../components/RadioCards";
 
 const Projects = () => {
   const projectsData = [
     {
-      type: "web-app",
+      type: "WEB-APP",
       title: "Weather App",
       image: "weather-app",
       link: "https://vaheedsk36.github.io/weather-app/",
       source: "https://github.com/vaheedsk36/weather-app",
     },
     {
-      type: "web-app",
+      type: "WEB-APP",
       title: "Landing Page",
       image: "landing-page",
       link: "https://vaheedsk36.github.io/demo-frontend-bootcamp",
       source: "https://github.com/vaheedsk36/demo-frontend-bootcamp",
     },
     {
-      type: "web-app",
+      type: "WEB-APP",
       title: "Guess Game",
       image: "guess-game",
       link: "https://vaheedsk36.github.io/guess-game",
       source: "https://github.com/vaheedsk36/guess-game",
     },
     {
-      type: "web-app",
+      type: "WEB-APP",
       title: "Notes App",
       image: "react-notes-app",
       link: "https://vaheedsk36.github.io/react-notes-app/",
       source: "https://github.com/vaheedsk36/react-notes-app",
     },
     {
-      type: "web-app",
+      type: "WEB-APP",
       title: "Dice Multiplayer",
       image: "dice-multiplayer",
       link: "https://vaheedsk36.github.io/dice-multiplayer",
       source: "https://github.com/vaheedsk36/dice-multiplayer",
     },
     {
-      type: "web-app",
+      type: "PROJECT",
       title: "Snake Game",
       image: "snake-game",
       link: "https://vaheedsk36.github.io/Snake-game",
       source: "",
     },
     {
-      type: "project",
+      type: "PROJECT",
       title: "Delta Assistant",
       image: "delta-assistant",
       link: "https://vaheedsk36.github.io/delta-assistant",
       source: "https://github.com/vaheedsk36/delta-assistant",
     },
     {
-      type: "web-app",
+      type: "WEB-APP",
       title: "Text Utility App",
       image: "text-utility-app",
       link: "https://vaheedsk36.github.io/text-utility-app/",
       source: "https://github.com/vaheedsk36/text-utility-app",
     },
   ];
+
+  const [selectedData, setSelectedData] = useState(projectsData);
+  const valSelectFn = (value) =>
+  setSelectedData(() => {
+    return value === "ALL"
+      ? projectsData
+      : projectsData.filter((data) => data.type === value);
+  });
+  const options = ["ALL", "WEB-APP", "PROJECT"];
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: "project",
+    defaultValue: "ALL",
+    onChange: valSelectFn,
+  });
+  const group = getRootProps();
+
   return (
     <>
       <div my={3} className="projects-container">
@@ -69,11 +86,22 @@ const Projects = () => {
           PROJECTS
         </Heading>
 
+        <HStack {...group}>
+          {options.map((value) => {
+            const radio = getRadioProps({ value });
+            return (
+              <RadioCard key={value} {...radio}>
+                {value}
+              </RadioCard>
+            );
+          })}
+        </HStack>
+
         <SimpleGrid
           spacing={4}
           templateColumns="repeat(auto-fill, minmax(350px, 1fr))"
         >
-          {projectsData.map((data, index) => {
+          {selectedData.map((data, index) => {
             return <ProjectCard {...{ data }} key={index} />;
           })}
         </SimpleGrid>
