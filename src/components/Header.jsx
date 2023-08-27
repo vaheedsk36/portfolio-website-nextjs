b07cf5d9178abcc11045cce2b9875035c991717b
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-08-28 01:16:44 +0530

    wip-navbar-responsive-design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,24 @@
 "use client";
 import React from "react";
 import HeaderTab from "./HeaderTab";
-import { Text } from "@chakra-ui/react";
+import { Text,Icon } from "@chakra-ui/react";
+import {
+  GiHamburgerMenu,
+} from "react-icons/gi";
 const Header = () => {
+
+  const headerClickhandler = ()=>{
+    document.body.classList.toggle('scroll-lock');
+  }
   return (
     <>
       <div className="nav-container">
-        <Text fontFamily="manlope" >VaheedSk36</Text>
-        {/* <HeaderTab /> */}
+        <div>
+        <Text fontFamily="manlope" >VaheedSk36 </Text>
+        <Icon as={GiHamburgerMenu} onClick={headerClickhandler}  />
+        </div>
+
+        <HeaderTab />
         <span className="nav-link-span"></span>
       </div>
     </>
