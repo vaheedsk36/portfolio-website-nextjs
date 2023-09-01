48bd134f4927429bdaceadef353b74fb09e6ef55
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-09-01 21:08:29 +0530

    interests-card-responsiveness-added

--- src/app/about/InterestsCard.jsx
+++ src/app/about/InterestsCard.jsx
@@ -1,21 +1,29 @@
 "use client"
 import React from "react";
-import {
-  Card,
-  Heading,
-  Icon
-} from "@chakra-ui/react";
+import { Card, Heading, Icon } from "@chakra-ui/react";
 
-const InterestsCard = ({interest,icon}) => {
+const InterestsCard = ({ interest, icon }) => {
+  
   return (
-    <>
-      <Card className="interests-card">
+    <Card
+      display="grid"
+      gridTemplateColumns="30% 70%"
+      alignItems="center"
+      width={["80vw","80vw","17.5rem"]}
+      height="5rem"
+      borderRadius="0"
+      backgroundColor="#213363"
+      fontSize="2.5rem"
+      padding="16px"
+      cursor="pointer"
+      transition="background-color .3s ease-in-out"
+      _hover={{
+        backgroundColor: "#213d8d",
+      }}
+    >
       <Icon as={icon} />
-          <Heading size="md">
-            {interest}
-          </Heading>
-      </Card>
-    </>
+      <Heading size="md">{interest}</Heading>
+    </Card>
   );
 };
 
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,74 +1,76 @@
 "use client";
 import React from "react";
-import { Heading, SimpleGrid, Image, Text } from "@chakra-ui/react";
+import { Heading, SimpleGrid, Image, Center, Box, Text } from "@chakra-ui/react";
 import InterestsCard from "./InterestsCard";
 import { interestsData } from "../../utils/constants";
 
 const Page = () => {
-
   return (
     <>
-      <div className="about-container">
-        <section id="about">
-          <Heading className="sub-heading" size="md" my={3} >
-            ABOUT
-          </Heading>
-          <div className="about-layout-container">
+      <Center m={5}>
+        <Box width="80vw" height="100%">
+          <section id="about">
+            <Heading className="sub-heading" size="md" my={3}>
+              ABOUT
+            </Heading>
+            <SimpleGrid columns={[1, null, 2]} gap={6} fontSize="1.1rem" marginBottom="4rem" templateColumns={[null,null,"30% 70%"]}>
             <Image
-              className="profile-image"
-              objectFit="shrink"
-              src="/images/profile.png"
-              alt="profile-image"
-            />
+                className="profile-image"
+                objectFit="shrink"
+                src="/images/profile.png"
+                alt="profile-image"
+              />
 
-            <div>
-              <Heading size="xl" mb={3} >
-                👋 Hey there
-              </Heading>
-              <br />
-              <p>
-                I&apos;m <span className="name">Vaheed</span>, a passionate software
-                developer. I specialize in{" "}
-                <span className="tech">Web Development</span>, crafting seamless
-                user experiences and robust backend solutions. With a knack for
-                problem-solving, I thrive on creating elegant code that makes an
-                impact. I&apos;m always exploring new tools and techniques to
-                stay at the forefront of the ever-evolving tech landscape.
-              </p>
-              <br />
-              <p>
-                📫 Open to collaborations and exciting projects, I&apos;m eager
-                to connect with fellow developers and tech enthusiasts.
-                Let&apos;s build something amazing together!
-              </p>
-              <p>
-                Happy coding! 🖥️
+              <Box >
+                <Heading size="xl" mb={3}>
+                  👋 Hey there
+                </Heading>
                 <br />
-              </p>
-            </div>
-          </div>
-        </section>
+                <Text>
+                  I&apos;m <span className="name">Vaheed</span>, a passionate
+                  software developer. I specialize in{" "}
+                  <span className="tech">Web Development</span>, crafting
+                  seamless user experiences and robust backend solutions. With a
+                  knack for problem-solving, I thrive on creating elegant code
+                  that makes an impact. I&apos;m always exploring new tools and
+                  techniques to stay at the forefront of the ever-evolving tech
+                  landscape.
+                </Text>
+                <br />
+                <Text>
+                  📫 Open to collaborations and exciting projects, I&apos;m
+                  eager to connect with fellow developers and tech enthusiasts.
+                  Let&apos;s build something amazing together!
+                </Text>
+                <Text>
+                  Happy coding! 🖥️
+                  <br />
+                </Text>
+              </Box>
+            </SimpleGrid>
+          </section>
 
-        <section id="interests">
-          <Heading className="sub-heading" size="md" my={3} >
-            INTERESTS
-          </Heading>
-          <SimpleGrid
-            spacing={4}
-            templateColumns="repeat(auto-fill, minmax(250px, 1fr))"
-          >
-            {interestsData.map((data, index) => {
-              return (
-                <InterestsCard
-                  interest={data.interest}
-                  icon={data.icon}
-                  key={index}
-                />
-              );
-            })}
-          </SimpleGrid>
-        </section>
-      </div>
+          <section id="interests">
+            <Heading className="sub-heading" size="md" my={3}>
+              INTERESTS
+            </Heading>
+            <SimpleGrid
+              spacing={4}
+              templateColumns="repeat(auto-fill, minmax(270px, 1fr))"
+            >
+              {interestsData.map((data, index) => {
+                return (
+                  <InterestsCard
+                    interest={data.interest}
+                    icon={data.icon}
+                    key={index}
+                  />
+                );
+              })}
+            </SimpleGrid>
+          </section>
+        </Box>
+      </Center>
     </>
   );
 };
