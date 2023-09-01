414d2c70cf6b08319ac4373d621ad5fae32c5530
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-09-02 02:01:56 +0530

    mapbox-added

---
+++ src/app/contact/MapBox.jsx
@@ -0,0 +1,55 @@
+import React, { useRef, useEffect } from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Box,
+} from "@chakra-ui/react";
+import mapboxgl from "mapbox-gl";
+
+const MapBox = ({ isOpen, onClose }) => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+
+  useEffect(() => {
+    // Map box was trying to find container and on initial load it was not getting it from the DOM, so it was not rendering.
+    setTimeout(() => {
+      if (isOpen) {
+        mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
+        map.current = new mapboxgl.Map({
+          container: mapContainer.current,
+          style: "mapbox://styles/mapbox/streets-v11",
+          center: [78.474, 17.385],
+          zoom: 8,
+        });
+      }
+    });
+
+    return () => {
+      if (map.current) {
+        map.current.remove();
+        map.current = null;
+      }
+    };
+  }, [isOpen]);
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} height="1000px" mx={2}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Location</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Box ref={mapContainer} className="map-container" height="400px" />
+        </ModalBody>
+        <ModalFooter></ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default MapBox;
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,12 +1,16 @@
 "use client";
-import React from "react";
+import React,{useState} from "react";
 import ContactForm from "./ContactForm";
-import { Heading, Icon, Stack, HStack, Text } from "@chakra-ui/react";
+import { Heading, Icon, Stack, Text, Tooltip,useDisclosure } from "@chakra-ui/react";
 import { IoIosMail } from "react-icons/io";
 import { ImLocation } from "react-icons/im";
+import { Link } from "@chakra-ui/next-js";
 import SocialIcons from "../../components/SocialIcons";
+import MapBox from "./MapBox";
 
 const Contact = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  
   return (
     <div className="container">
       <div className="contact-container">
@@ -24,20 +28,30 @@ const Contact = () => {
           </Heading>
 
           <Stack marginTop="4rem">
-            <Text className="contact-text-span" my={3}>
-              <Icon as={IoIosMail} mx={2} />
-              <span>vaheedsk36@gmail.com</span>
-            </Text>
-            <Text className="contact-text-span" my={3}>
-              <Icon as={ImLocation} mx={2} />
-              <span>Hyderabad, India</span>
-            </Text>
+            <Tooltip label="Send Mail">
+              <Link
+                href="mailto:vaheedsk36@gmail.com"
+                target="_blank"
+                className="contact-text-span"
+                my={3}
+                _hover={{ textDecoration: "none" }}
+              >
+                <Icon as={IoIosMail} mx={2} />
+                <Text>vaheedsk36@gmail.com</Text>
+              </Link>
+            </Tooltip>
+
+                <Text className="contact-text-span" my={3} onClick={onOpen}>
+                  <Icon as={ImLocation} mx={2} />
+                  <span>Hyderabad, India</span>
+                </Text>
           </Stack>
-          <SocialIcons/>
+          <SocialIcons />
         </div>
 
         <ContactForm />
       </div>
+      <MapBox {...{isOpen, onOpen, onClose }}/>
     </div>
   );
 };
