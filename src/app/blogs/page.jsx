7a9f6829e45e18533fb0dac65e5f4d06f2f6201f
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-09-04 22:09:04 +0530

    blogs-responsiveness-added

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import ArticlesCard from "./ArticlesCard";
-import { Heading, Box, Center } from "@chakra-ui/react";
+import { Heading, Box, Center, SimpleGrid } from "@chakra-ui/react";
 import NewsLetterComponent from "./NewsLetterComponent";
 import { getArticlesData } from "../../utils/utils";
 import { FidgetSpinner } from "react-loader-spinner";
@@ -19,13 +19,13 @@ const Blogs = () => {
 
   return (
     <>
-      <Center m={5}>
+      <Center my={5}>
         {isLoading ? (
           <Box
-          position="absolute"
-          display="flex"
-          top="40vh"
-          justifyContent="center"
+            position="absolute"
+            display="flex"
+            top="40vh"
+            justifyContent="center"
           >
             <FidgetSpinner
               visible={true}
@@ -38,18 +38,25 @@ const Blogs = () => {
             />
           </Box>
         ) : (
-          <Box width="95%" height="100%">
+          <Box width={["87vw", "87vw", "87vw"]} height="100%">
             <Heading className="sub-heading" size="md">
               Blogs
             </Heading>
 
             <NewsLetterComponent />
 
-            <div className="blogs-grid-container">
+            <SimpleGrid
+              spacing={4}
+              templateColumns={[
+                "repeat(auto-fill, minmax(250px, 1fr))",
+                "repeat(auto-fill, minmax(250px, 1fr))",
+                "repeat(auto-fill, minmax(300px, 1fr))",
+              ]}
+            >
               {articlesData.map((data, index) => (
                 <ArticlesCard data={data} key={index} />
               ))}
-            </div>
+            </SimpleGrid>
           </Box>
         )}
       </Center>
