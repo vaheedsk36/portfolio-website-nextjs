15e8a7d6f51fb7939e961802c725c8fe0f7f5545
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-10-16 03:12:56 +0530

    Resume-added

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -2,57 +2,54 @@
 import React from "react";
 import {
     Center,
-    Box
+    Box,
+    Button
   } from "@chakra-ui/react";
+  import { AiOutlineDownload } from "react-icons/ai";
 
 const Resume = () => {
+
   return (
     <>
-    <Center>
-      <Box width={["90vw", "90vw", "80vw"]} height="100%" marginBottom="4rem">
-
-      <div
-        style={{
-          position: "relative",
-          width: "100%",
-          height: 0,
-          paddingTop: "141.4286%",
-          paddingBottom: 0,
-          boxShadow: "0 2px 8px 0 rgba(63, 69, 81, 0.16)",
-          marginTop: "1.6em",
-          marginBottom: "0.9em",
-          overflow: "hidden",
-          borderRadius: "8px",
-          willChange: "transform"
-        }}
-      >
-        <iframe
-          title="Resume"
-          loading="lazy"
-          style={{
-            position: "absolute",
-            width: "100%",
-            height: "100%",
-            top: 0,
-            left: 0,
-            border: "none",
-            padding: 0,
-            margin: 0
-          }}
-          src="https://www.canva.com/design/DAE02jCrTBo/view?embed"
-          allowFullScreen
-        ></iframe>
-      </div>
-      <a
-        href="https://www.canva.com/design/DAE02jCrTBo/view?utm_content=DAE02jCrTBo&utm_campaign=designshare&utm_medium=embeds&utm_source=link"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        CV_Masood Akhtar Vaheed
-      </a>{" "}
-      by Masood Akhtar Vaheed
-
-      </Box>
+      <Center>
+        <Box width={["90vw", "90vw", "80vw"]} height="100%" marginTop="2rem" marginBottom="4rem">
+        <Button as="a" href="/files/vaheed_cv.pdf" colorScheme="teal" size="sm" download="Masood_Akhtar_Vaheed_CV">
+              Download&nbsp;
+              <AiOutlineDownload fill="black"/>
+        </Button>
+          <div
+            style={{
+              position: "relative",
+              width: "100%",
+              height: 0,
+              paddingTop: "141.4286%",
+              paddingBottom: 0,
+              boxShadow: "0 2px 8px 0 rgba(63, 69, 81, 0.16)",
+              marginTop: "1em",
+              marginBottom: "0.9em",
+              overflow: "hidden",
+              borderRadius: "8px",
+              willChange: "transform",
+            }}
+          >
+            <iframe
+              title="Resume"
+              loading="lazy"
+              style={{
+                position: "absolute",
+                width: "100%",
+                height: "100%",
+                top: 0,
+                left: 0,
+                border: "none",
+                padding: 0,
+                margin: 0,
+              }}
+              src="https://www.canva.com/design/DAE02jCrTBo/view?embed"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </Box>
       </Center>
     </>
   );
