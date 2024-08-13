e90d52c826aca0276e8d91fbe266e8042cb788e8
Imran Imtiaz <imranimtiaz48@hotmail.com>
2024-08-13 07:58:08 +0400

    Update page.jsx

--- src/app/skills/page.jsx
+++ src/app/skills/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import {
   Card,
   CardBody,
@@ -12,47 +12,42 @@ import {
 import { skillsData } from "../../utils/constants";
 import { RevealWrapper } from "next-reveal";
 
-const Skills = () => {
-
-  return (
-    <Center height="70vh" width="100vw">
-      <Box width="80vw" height="100%">
-        <Heading className="sub-heading" size="md" my={3}>
-          SKILLS
-        </Heading>
-        <RevealWrapper className="load-hidden" delay={300}>
+const Skills = () => (
+  <Center height="70vh" width="100vw">
+    <Box width="80vw" height="100%">
+      <Heading className="sub-heading" size="md" my={3}>
+        SKILLS
+      </Heading>
+      <RevealWrapper className="load-hidden" delay={300}>
         <SimpleGrid
           marginTop="4rem"
           spacing={5}
           templateColumns="repeat(auto-fill, minmax(135px, 1fr))"
           paddingBottom="2rem"
         >
-          {skillsData.map((data, index) => {
-            return (
-              <Card
-                key={index}
-                backgroundColor="blueTheme.card"
-                transition="all .2s ease-in-out"
-                _hover={{
-                  cursor: "pointer",
-                  backgroundColor: "blueTheme.cardHover",
-                  transform:"scale(1.05)"
-                }}
-              >
-                <CardBody display="flow" alignItems="center" textAlign="center">
-                  <Icon as={data.icon} width="55px" height="55px" />
-                  <Heading my={1} textAlign="center" fontSize= "1.2rem">
-                    {data.name}
-                  </Heading>
-                </CardBody>
-              </Card>
-            );
-          })}
+          {skillsData.map((data, index) => (
+            <Card
+              key={index}
+              bg="blueTheme.card"
+              transition="all .2s ease-in-out"
+              _hover={{
+                cursor: "pointer",
+                bg: "blueTheme.cardHover",
+                transform: "scale(1.05)",
+              }}
+            >
+              <CardBody display="flex" flexDirection="column" alignItems="center" textAlign="center">
+                <Icon as={data.icon} w={14} h={14} />
+                <Heading my={1} fontSize="1.2rem">
+                  {data.name}
+                </Heading>
+              </CardBody>
+            </Card>
+          ))}
         </SimpleGrid>
-        </RevealWrapper>
-      </Box>
-    </Center>
-  );
-};
+      </RevealWrapper>
+    </Box>
+  </Center>
+);
 
 export default Skills;
